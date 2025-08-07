49306f121b5d368248a00d295c46a8a8026b7fc5
kevinfst <kevinst2610@gmail.com>
2025-08-06 23:39:54 -0300

    chore: initial project setup for PHP Learning

---
+++ ex000/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 01 de PHP</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            echo "Olá, Mundo! \u{1F30E}";
+        ?>
+    </h1>
+    <p>Criando um código PHP</p>
+</body>
+</html>
