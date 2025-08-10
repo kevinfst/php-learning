bc83d7e63831e2d28df2a8aea368c30d9b52b7e1
kevinfst <kevinst2610@gmail.com>
2025-08-09 23:56:16 -0300

    feat: PHP primitive types

---
+++ ex004/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <h1>Tipos primitivos do PHP</h1>
+    <?php 
+        // $num = 300;
+        // echo  "O valor da variável é $num";
+
+        // var_dump() Para saber o tipo da variável e o que tem 
+        // $v = 300;
+        // var_dump($v); dentro da mesma.
+
+        // 3e2 -> 3 x 10(2) Basicamente seria 3 ELEVADO ^ a 2 = 300
+        // $num = 3e2; 
+        // echo "O valor é $num";
+
+        // Exemplo de coerção
+        // $num = (float) "950";
+        // var_dump($num);
+
+        // Para uma variável do tipo boolean se tentar exibir o resultado, para TRUE o valor é 1 e o valor de FALSE é vazio
+        // $casado = true;
+        //var_dump($casado);
+        // echo "O valor para casado é $casado";
+
+        echo "Apenas testes e testes para aprendizado!";
+    ?>
+</body>
+</html>
