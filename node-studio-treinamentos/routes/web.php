a478cf5a71980274dc6a5588ab2e7ee90ad51d05
kevinfst <kevinst2610@gmail.com>
2025-08-19 20:45:23 -0300

    feat: add parameter passing

--- node-studio-treinamentos/routes/web.php
+++ node-studio-treinamentos/routes/web.php
@@ -18,5 +18,9 @@ Route::match(['get', 'put', 'delete'], '/match', function() {
     return "Permite apenas acessos definidos";
 });
 
-
-
+// O ? depois de {cat} geralmente indica que o parâmetro é opcional (ou seja, pode vir ou não).
+// A atribuição default = ' '  é o valor padrão: se ninguém passar nada, ele assume string vazia.
+// Resumidamente: ? = opcional | = ' ' = valor padrão vazio.
+Route::get('/produto/{id}/{cat?}', function($id, $cat = ''){
+    return "O id do produto é: ".$id."<br>"."A categoria é: ".$cat;
+});
