6dfe0aef803707c1b0a341208db1651c4ebfdead
kevinfst <kevinst2610@gmail.com>
2025-08-19 21:14:29 -0300

    feat: add redirect and view handling

--- node-studio-treinamentos/routes/web.php
+++ node-studio-treinamentos/routes/web.php
@@ -6,13 +6,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/praedium', function() {
-    return view('site/praedium');
-});
-
 Route::any('/any', function() {
     return "Permite todo tipo de acesso HTTP (put, delete, get, post)";
 });
+
+/* OBS, na linha 40 tem outra forma de utilizar.
+Route::get('/praedium', function() { 
+    return view('site/praedium');
+});
+*/
 
 Route::match(['get', 'put', 'delete'], '/match', function() {
     return "Permite apenas acessos definidos";
@@ -24,3 +26,17 @@ Route::match(['get', 'put', 'delete'], '/match', function() {
 Route::get('/produto/{id}/{cat?}', function($id, $cat = ''){
     return "O id do produto é: ".$id."<br>"."A categoria é: ".$cat;
 });
+
+/*
+Route::get('/sobre', function() {
+    return redirect('/praedium');
+});
+*/
+
+// Ou dessa forma
+Route::redirect('/sobre', '/praedium'); // Código menor, fazendo a mesma função que basicamente seria redirecionar de uma rota para a outra.
+
+// Código da linha 14 de forma simplificada.
+Route::view('/praedium', 'site/praedium'); // Como estavamos só renderizando uma view, não tinha necessidade de colocar um return dentro de uma função de callback. Podemos renderizar uma view baseado na rota, se for só essa tarefa a ser realizada.
+
+// O início '/praedium' seria a ROTA e 'site/praedium' seria a VIEW praedium que está dentro de site(pasta).
